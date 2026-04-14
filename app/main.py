77a729aa1c9b3a28d66fcc006de51b0017723862
Ruslan <stadnikruslan168@gmail.com>
2026-04-14 18:34:29 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -6,26 +6,24 @@ API_KEY = os.environ.get("API_KEY")
 if not API_KEY:
     raise SystemExit("API_KEY is required")
 
+CITY = "Paris"
 URL = "http://api.weatherapi.com/v1/current.json"
-params = {"key": API_KEY, "q": "Paris"}
-
-try:
-    resp = requests.get(URL, params=params, timeout=10)
-    resp.raise_for_status()
-except requests.RequestException as e:
-    print("Request error", e)
-    raise SystemExit(1)
-
-data = resp.json()
 
 
 def get_weather() -> None:
-    local = data.get("location", {})
-    current = data.get("current", {})
-    print(f"{local['name']}/{local['country']} "
-          f"{local['localtime']} "
-          f"Weather: {current['temp_c']} Celsius "
-          f"{current['condition']['text']}")
+    params = {"key": API_KEY, "q": CITY}
+    try:
+        resp = requests.get(URL, params=params, timeout=10)
+        resp.raise_for_status()
+    except requests.RequestException as e:
+        print("Request error", e)
+        raise SystemExit(1)
+
+    data = resp.json()
+    loc = data.get("location", {})
+    cur = data.get("current", {})
+    print(f"{loc.get('name')}/{loc.get('country')} {loc.get('localtime')} "
+          f"Weather: {cur.get('temp_c')}°C, {cur.get('condition', {}).get('text')}")
 
 
 if __name__ == "__main__":
