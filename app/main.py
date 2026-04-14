d629d24c60c8eca38364ea1adc49c33de9ebf2ed
Ruslan <stadnikruslan168@gmail.com>
2026-04-14 17:49:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
+import os
+import requests
+
+
+API_key = os.environ.get("API_KEY")
+if not API_key:
+    raise SystemExit("API_KEY is required")
+url = f"http://api.weatherapi.com/v1/current.json?key={API_key}&q=Paris"
+
+resp = requests.get(url)
+data = resp.json()
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    print(f"{data['location']['name']}/{data['location']['country']} "
+          f"{data['location']['localtime']} "
+          f"Weather: {data['current']['temp_c']} Celsius, "
+          f"{data['current']['condition']['text']}"
+    )
 
 
 if __name__ == "__main__":
